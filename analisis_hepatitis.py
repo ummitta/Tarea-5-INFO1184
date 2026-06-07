cd17a85d1f6fcfba0b2d308f74e45690455a857d
max <maxmora401@gmail.com>
2026-06-06 23:58:09 -0400

    Carga el dataset de hepatitis desde UCI

--- analisis_hepatitis.py
+++ analisis_hepatitis.py
@@ -3,3 +3,18 @@ from ucimlrepo import fetch_ucirepo
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+
+
+def main() -> None:
+    hepatitis = fetch_ucirepo(id=46)
+
+    X = hepatitis.data.features
+    y = hepatitis.data.targets
+
+    df = pd.concat([X, y], axis=1)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
