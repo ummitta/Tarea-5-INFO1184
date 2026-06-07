482dcdda3376666cb1439140f58e100266049f90
max <maxmora401@gmail.com>
2026-06-07 00:28:36 -0400

    Agrega comparación entre edad y supervivencia y mas cosas

--- analisis_hepatitis.py
+++ analisis_hepatitis.py
@@ -5,6 +5,40 @@ import pandas as pd
 import numpy as np
 
 
+# Gráficos
+def distribucion_edades(df):
+    plt.figure(figsize=(8, 5))
+
+    plt.hist(df["Age"], bins=10)
+
+    plt.title("Distribución de edades")
+    plt.xlabel("Edad")
+    plt.ylabel("Frecuencia")
+
+    plt.show()
+    return
+
+
+def edad_supervivencia(df):
+    plt.figure(figsize=(6, 5))
+
+    muere = df[df["Class"] == "Muere"]["Age"]
+    sobrevive = df[df["Class"] == "Sobrevive"]["Age"]
+
+    plt.boxplot(
+        [muere, sobrevive],
+        tick_labels=["Muere", "Sobrevive"]
+    )
+
+    plt.title("Edad según resultado del paciente")
+    plt.xlabel("Resultado")
+    plt.ylabel("Edad")
+    plt.grid(True)
+
+    plt.show()
+    return
+
+
 # Etapas de la metodología CRISP-DM
 def comprension_datos(df) -> None:
     print(df.info())
@@ -62,6 +96,27 @@ def preparacion_datos(df) -> None:
     print("\nValores faltantes después de la preparación:")
     print(df.isnull().sum())
     print(f"{'':=^32}")
+
+    # Cambia los valores por etiquetas más fáciles de interpretar
+    df["Class"] = df["Class"].map({
+        1: "Muere",
+        2: "Sobrevive"
+    })
+
+    df["Sex"] = df["Sex"].map({
+        1: "Hombre",
+        2: "Mujer"
+    })
+
+    for col in vars_categoricas[1:]:
+        df[col] = df[col].map({
+            1: "No",
+            2: "Si"
+        })
+
+    # Gráficos
+    distribucion_edades(df)
+    edad_supervivencia(df)
 
     return
 
